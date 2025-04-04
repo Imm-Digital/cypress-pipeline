37f85b00e0744b4facdaa30717fa538382112500
henrique-impulse <henriquesantos@sejaimpulse.com>
2025-04-04 10:44:08 -0300

    feat: edit var

--- cypress/e2e/funil.cy.js
+++ cypress/e2e/funil.cy.js
@@ -1,19 +1,17 @@
 describe("Validação de requests do Google Tag Manager e Google Analytics", () => {
-  let utms, formfields, domain, vlsId, pitch, checkout;
+  let utms, formfields, vlsId, pitch, checkout;
 
   before(() => {
-    cy.fixture("cypress.env.funil.json").then((env) => {
-      Cypress.env(env);
-      utms =
-        "?utm_source=GoogleAds&utm_medium=21447195831&utm_campaign=167489459667&utm_content=g&utm_term=profissional+de+transcricao&gad_source=1&gclid=CjwKCAiA5Ka9BhB5EiwA1ZVtvJS9Wm-DY1bQmylo1ioqc5T3iCi6BeAPuFYJhO4dKvTLz4xA5u7oexoCAq4QAvD_BwE&impulse_id=";
-      formfields =
-        "&name=John&email=test%40gmail.com&phone=9999999999&full_name=John+Doe";
-      domain = Cypress.env("DOMINIO_PROJETO");
-      vlsId = Cypress.env("VSL_ID");
-      pitch = Cypress.env("VSL_PITCH");
-      checkout = Cypress.env("CHECKOUT_LINK");
-    });
+    utms =
+      "?utm_source=GoogleAds&utm_medium=21447195831&utm_campaign=167489459667&utm_content=g&utm_term=profissional+de+transcricao&gad_source=1&gclid=CjwKCAiA5Ka9BhB5EiwA1ZVtvJS9Wm-DY1bQmylo1ioqc5T3iCi6BeAPuFYJhO4dKvTLz4xA5u7oexoCAq4QAvD_BwE&impulse_id=";
+    formfields =
+      "&name=John&email=test%40gmail.com&phone=9999999999&full_name=John+Doe";
+  
+    vlsId = Cypress.env("VSL_ID");
+    pitch = Cypress.env("VSL_PITCH");
+    checkout = Cypress.env("CHECKOUT_LINK");
   });
+
   beforeEach(() => {
     cy.intercept(
       "GET",
@@ -23,25 +21,24 @@ describe("Validação de requests do Google Tag Manager e Google Analytics", () 
       "GET",
       "https://www.googletagmanager.com/gtag/js?id=G-65XLVDHJC2"
     ).as("gtag");
-    cy.visit(domain + utms);
+    cy.visit("/" + utms); // <-- Agora usando caminho relativo
   });
 
-  it("Verifica se GTM E GA carregaram", () => {
+  it("Verifica se GTM e GA carregaram", () => {
     cy.wait("@gtm").its("response.statusCode").should("eq", 200);
     cy.wait("@gtag").its("response.statusCode").should("eq", 200);
   });
 
   it("Verifica se o Impulse ID foi salvo no Local Storage", () => {
     cy.wait(2000);
-
     cy.window().then((win) => {
       const impulseId = win.localStorage.getItem("impulse_id");
-      expect(impulseId).to.not.be.null; // Verifica se o impulseId não é nulo
+      expect(impulseId).to.not.be.null;
     });
   });
 
-  it("Verifica a passagem de UTMs P1 > P2 > PV > Checkout e Fields Forms P2 > PV > Checkout", () => {
-    cy.get("button").first().should("be.visible").click(); // ALTERAR, DEPENDENDO DA CATEGORIA DO FUNIL
+  it("Verifica a passagem de UTMs e form fields entre as etapas", () => {
+    cy.get("button").first().should("be.visible").click(); // ← ajuste se necessário
 
     cy.wait(2000);
 
@@ -49,18 +46,16 @@ describe("Validação de requests do Google Tag Manager e Google Analytics", () 
       win.localStorage.setItem(vlsId, pitch.toString());
       const impulseId = win.localStorage.getItem("impulse_id");
       cy.wrap(impulseId).as("impulseId");
-      return;
     });
 
     cy.get("@impulseId").then((impulseId) => {
-      cy.url().should("include", domain + "p2" + utms + impulseId);
+      cy.url().should("include", "/p2" + utms + impulseId);
     });
 
     cy.get("select").first().select(2);
     cy.get("select").eq(1).select(1);
     cy.get("select").eq(2).select(1);
 
-    // Type into the input fields
     cy.get("input").first().type("test");
     cy.get("select").eq(3).select(1);
     cy.get("select").eq(4).select(1);
@@ -71,8 +66,9 @@ describe("Validação de requests do Google Tag Manager e Google Analytics", () 
     cy.get("button").first().click();
 
     cy.get("@impulseId").then((impulseId) => {
-      cy.url().should("include", domain + "pv" + utms + impulseId + formfields);
+      cy.url().should("include", "/pv" + utms + impulseId + formfields);
     });
+
     cy.wait(2000);
 
     cy.get(".smartplayer-resume__play > .smartplayer-resume__icon")
@@ -84,7 +80,7 @@ describe("Validação de requests do Google Tag Manager e Google Analytics", () 
     cy.get(`a[href*="${checkout}"]`)
       .should("be.visible")
       .then(($link) => {
-        cy.visit($link.attr("href")); // Visita o href do elemento
+        cy.visit($link.attr("href")); // ← visita o link de checkout externo
       });
 
     cy.get("@impulseId").then((impulseId) => {
